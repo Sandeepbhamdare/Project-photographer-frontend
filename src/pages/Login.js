cdf4559103c782aa36e2b144601653ee267dd415
Sandeep Bhamdare <sandeepbhamdare3161@gmail.com>
2024-04-27 13:11:44 +0530

    Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,48 @@
 import { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import Loader from "../components/Loader";
+import "react-toastify/dist/ReactToastify.css";
 
 const Login = () => {
 
     const [userLogin, setUserLogin] = useState({ email: "", password: "" })
+    const { email, password } = userLogin;
+    const [isLoading, setIsLoading] = useState(false)
+
+    const navigate = useNavigate()
 
     const onChange = (e) => {
         setUserLogin({ ...userLogin, [e.target.name]: e.target.value });
     }
 
-    const handleLogin=(e)=>{
-          e.preventDefault()
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        setIsLoading(true)
+        const response = await fetch('http://localhost:3002/auth/login', {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({ email, password })
+        })
+        const data = await response.json()
+        setIsLoading(false)
+        if (data.status) {
+            console.log(data.data)
+            const userData = JSON.stringify(data.data)
+            localStorage.setItem('userData', userData)
+            navigate("/")
+
+        } else {
+            toast.error(data.message)
+        }
+    }
+
+    const handleReset = (e) => {
+        e.preventDefault()
+        setUserLogin({ email: "", password: "" })
     }
 
     return (
@@ -24,7 +55,7 @@ const Login = () => {
                     <h1 className="galdeano-regular">Log in</h1>
                     <div>
                         <p >E-mail</p>
-                        <input type="email" name="email" value={userLogin.email } onChange={onChange} />
+                        <input type="email" name="email" value={userLogin.email} onChange={onChange} />
                     </div>
                     <div>
                         <p>Password</p>
@@ -33,11 +64,14 @@ const Login = () => {
 
                     <span className="btn-container">
                         <button className="auth-btn inder-regular" onClick={handleLogin}>Login</button>
-                        <button className="reset-btn">reset</button>
+                        <button className="reset-btn" onClick={handleReset}>reset</button>
                     </span>
                     <p>Don’t have an account ? <Link to="/signup">Sign Up</Link> </p>
                 </form>
             </div>
+
+            {isLoading ? <Loader /> : ""}
+            <ToastContainer />
         </>
     )
 }
